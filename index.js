1ea45f323b92fdef02ebcbbe37e62a0af2b7cf9f
Yacine <hadjaj.yacine.sio@gmail.com>
2024-01-29 11:35:04 +0100

    Correction code JS (suppression session)

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ function showTime(){
     
     h = (h < 10) ? "0" + h : h;
     m = (m < 10) ? "0" + m : m;
-    s = (s < 10) ? "0" + s : s;
+    s = (s < 10) ? "0" + s : s;   
     
-    var time = h + ":" + m + ":" + s + " " + session;
+    var time = h + ":" + m + ":" + s;
     document.getElementById("DigitalCLOCK").innerText = time;
     document.getElementById("DigitalCLOCK").textContent = time;
     
