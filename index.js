62a09c15c8fd6081881359b999becf4f65fea68d
Yacine <hadjaj.yacine.sio@gmail.com>
2024-01-29 10:43:36 +0100

    initialisation du projet avec JavaScript

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-<script>
-    alerte('Projet Anna ');
-</script>
+// Original Code By Webdevtrick ( https://webdevtrick.com )
+function showTime(){
+    var date = new Date();
+    var h = date.getHours();
+    var m = date.getMinutes();
+    var s = date.getSeconds();
+    
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+    
+    var time = h + ":" + m + ":" + s + " " + session;
+    document.getElementById("DigitalCLOCK").innerText = time;
+    document.getElementById("DigitalCLOCK").textContent = time;
+    
+    setTimeout(showTime, 1000);
+    
+}
+showTime();
